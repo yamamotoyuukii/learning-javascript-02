de803c84e2cfd7f56cd6370e71383fb670eb6af1
yamamotoyuuki <therizinosaurus514@gmail.com>
2019-07-05 09:37:52 +0900

    6/28 課題

--- 01.js
+++ 01.js
@@ -1,4 +1,14 @@
 // FizzBuzz
+for (let i = 1; i < 16; i++)
+... if ((i % 3=== 0) && (i % 5)=== 0){
+... console.log("FizzBuzz");
+... }else if (i % 3 === 0){
+... console.log("Fizz");
+... }else if (i % 5 === 0){
+... console.log("Buzz");
+... }else{
+... console.log(i)
+... }
 
 // サンプル: "1" という文字列を出力
 console.log("1");
